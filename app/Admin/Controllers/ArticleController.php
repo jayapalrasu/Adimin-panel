50840513dc3d4ab2a487938d58b8e89156e2f9a4
jayapal <jayapal@trasol.in>
2022-08-04 15:58:13 +0530

    first commit

---
+++ app/Admin/Controllers/ArticleController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Admin\Controllers;
+
+use App\Models\Article;
+use Encore\Admin\Controllers\AdminController;
+use Encore\Admin\Form;
+use Encore\Admin\Grid;
+use Encore\Admin\Show;
+
+class ArticleController extends AdminController
+{
+    /**
+     * Title for current resource.
+     *
+     * @var string
+     */
+    protected $title = 'Article';
+
+    /**
+     * Make a grid builder.
+     *
+     * @return Grid
+     */
+    protected function grid()
+    {
+        $grid = new Grid(new Article());
+
+        $grid->column('id', __('Id'));
+        $grid->column('title', __('title'));
+        $grid->column('content', __('content'));
+        $grid->column('created_at', __('Created at'));
+        $grid->column('updated_at', __('Updated at'));
+
+        return $grid;
+    }
+
+    /**
+     * Make a show builder.
+     *
+     * @param mixed $id
+     * @return Show
+     */
+    protected function detail($id)
+    {
+        $show = new Show(Article::findOrFail($id));
+
+        $show->column('id', __('Id'));
+        $show->column('title', __('title'));
+        $show->column('content', __('content'));
+        $show->column('created_at', __('Created at'));
+        $show->column('updated_at', __('Updated at'));
+
+        return $show;
+    }
+
+    /**
+     * Make a form builder.
+     *
+     * @return Form
+     */
+    protected function form()
+    {
+        $form = new Form(new Article());
+
+
+        $form->text('title', __('title'));
+        $form->text('content', __('content'));
+
+
+        return $form;
+    }
+}
